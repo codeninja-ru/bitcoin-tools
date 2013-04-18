68107f492632c8fc3ec00832032008da72a5928d
codeninja <vital@ubuntu-home.(none)>
2013-04-18 12:49:53 +1100

    вжи вжи

--- btc.py
+++ btc.py
@@ -3,7 +3,8 @@ import urllib2
 import json
 import time
 
-fee = .998 * .995 * .998
+fee1 = .998 * .995 * .998
+fee2 = .998 * .998 * .998
 
 def get(url):
   req = urllib2.Request(url)
@@ -14,24 +15,51 @@ def get(url):
 
 def get_depth(pair):
   depth = get("https://btc-e.com/api/2/%s/depth" % pair)
-  return (depth['asks'][0][0], depth['bids'][0][0])
+  asks = depth['asks']
+  bids = depth['bids']
+  #asks = filter(lambda x: x[1] >= 1, asks)
+  #bids = filter(lambda x: x[1] >= 1, bids)
+  return (asks[0][0], bids[0][0], asks[0][1], bids[0][1])
+
+def calc_profit(cur1, cur2, cur3, fee):
+  n1, c1 = cur1
+  n2, c2 = cur2
+  n3, c3 = cur3
+  total = (c1[1] * c2[1] * fee) / c3[0]
+  min_amount = min(c1[3], c2[3], c3[2])
+  if total > 1:
+    t = time.strftime('%Y-%m-%d %H:%M:%S')
+    print "%.4f <- Fuck yeah! (%s)" % (total, t)
+    print "%s (%.1f) -> %s (%.1f) -> %s (%.1f), min amount -> %.2f" % (n1, c1[1], n2, c2[1], n3, c3[0], min_amount)
+  return total
 
 def process():
-  btc_usd = get_depth('btc_usd')
-  usd_rur = get_depth('usd_rur')
-  btc_rur = get_depth('btc_rur')
+  def rev(t):
+    name = '/'.join(t[0].split('/')[::-1])
+    return (name, (1/t[1][1], 1/t[1][0]))
 
-  total = (btc_usd[1] * usd_rur[1] * fee) / btc_rur[0]
-  if total > 1:
-    print "%.4f <- Fuck yeah!" % total
-    print "BTC/USD (%.1f) -> USD/RUR (%.1f) -> BTC/RUR (%.1f)" % (btc_usd[1],usd_rur[1], btc_rur[0])
+  btc_usd = ('BTC/USD', get_depth('btc_usd'))
+  usd_rur = ('USD/RUR', get_depth('usd_rur'))
+  btc_rur = ('BTC/RUR', get_depth('btc_rur'))
+  ltc_usd = ('LTC/USD', get_depth('ltc_usd'))
+  ltc_rur = ('LTC/RUR', get_depth('ltc_rur'))
+  ltc_btc = ('LTC/BTC', get_depth('ltc_btc'))
+
+  calc_profit(btc_usd, usd_rur, btc_rur, fee1)
+  calc_profit(ltc_usd, usd_rur, ltc_rur, fee1)
+  calc_profit(ltc_btc, btc_rur, ltc_rur, fee2)
+  calc_profit(ltc_btc, btc_usd, ltc_usd, fee2)
+
+  #calc_profit(rev(btc_rur), rev(usd_rur), btc_usd, fee2)
+
 
 def main():
   while True:
     try:
       process()
-    except:
+    except Exception as ex:
       print "error. ups..."
+      print ex
     time.sleep(3)
 
 
